0fc8de8e9b298e941230269c9318c7960eb965b3
chenhongji12 <1794606767@qq.com>
2017-06-15 06:31:59 +0800

    sinal test4

--- sinalCtrl.c
+++ sinalCtrl.c
@@ -1,17 +1,17 @@
-#include <stdio.h>
-#include <sigal.h>
+#include <signal.h>
 #include <unistd.h>
-
-void Stop(void)
-{
-	printf("access ctrl+c!");
-	_exit(1);
-}
-int main()
-{
-	int res;
-
-	signal(SIGINT, Stop);
-	while(1);
-	return 0;
-}
+#include<stdio.h>  
+void ctrlhandler( int a )  
+{  
+ printf("you have press ctrl+c /n");  
+ /* close 相关操作*/  
+ getchar();  
+ exit(0);  
+}  
+int main()  
+{  
+ signal(SIGINT,ctrlhandler);  
+ /*其他操作*/  
+ getchar();  
+ return 1;  
+}  
