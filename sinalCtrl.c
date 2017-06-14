a7f8146d47635719e0e19241547988bc090d06ef
chenhongji12 <1794606767@qq.com>
2017-06-15 06:27:54 +0800

    sinal test123

--- sinalCtrl.c
+++ sinalCtrl.c
@@ -1,2 +1,17 @@
 #include <stdio.h>
 #include <sigal.h>
+#include <unistd.h>
+
+void Stop(void)
+{
+	printf("access ctrl+c!");
+	_exit(1);
+}
+int main()
+{
+	int res;
+
+	signal(SIGINT, Stop);
+	while(1);
+	return 0;
+}
